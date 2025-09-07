e2378ec872c0bced05cceba5c8f0040e7e00029f
TridibeshSam31 <tridisam651@gmail.com>
2025-09-08 00:37:48 +0530

    open ai

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,18 @@
+//here we will add ai suggestions means using ui we will suggest the user visiting the platform what they should write
+//for that we will integrate openAI in that we will give all the suggesting messages in form of array 
+import { openai } from '@ai-sdk/openai';
+import { streamText, UIMessage, convertToModelMessages } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    model: openai('gpt-4o'),
+    messages: convertToModelMessages(messages),
+  });
+
+  return result.toUIMessageStreamResponse();
+}
