32bad620dc513dc01b0c8fd95b3a9f6bc604fb43
TridibeshSam31 <tridisam651@gmail.com>
2025-12-05 00:55:44 +0530

    assignment

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,3 +1,9 @@
+//assignment part 
+// the u/[username] folder should contain a page where anonymous users can send messages to a specific user.
+//humko link se username ko extract krna hai yaani params.username ko extract krna hai 
+//ai suggestions dene hai user ko 
+
+
 import React from 'react'
 
 const page = () => {
